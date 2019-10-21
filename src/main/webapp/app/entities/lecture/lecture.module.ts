ba5a73ab95786fc100d7526653a3964cf84d751a
Beatris Burdeva <beatris.burdeva@tum.de>
2019-10-21 22:40:30 +0200

    Added markdown for the lectures' description

--- src/main/webapp/app/entities/lecture/lecture.module.ts
+++ src/main/webapp/app/entities/lecture/lecture.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ArtemisSharedModule } from 'app/shared';
-import { LectureAttachmentsComponent, LectureComponent, LectureDetailComponent, lectureRoute, LectureUpdateComponent } from './';
+import {
+    LectureAttachmentsComponent,
+    LectureComponent,
+    LectureDetailComponent,
+    lectureRoute,
+    LectureService,
+    LectureUpdateComponent
+} from './';
 import { FormDateTimePickerModule } from 'app/shared/date-time-picker/date-time-picker.module';
 import { ArtemisConfirmButtonModule } from 'app/components/confirm-button/confirm-button.module';
+import { ArtemisMarkdownEditorModule } from 'app/markdown-editor';
 
 const ENTITY_STATES = [...lectureRoute];
 
 @NgModule({
-    imports: [ArtemisSharedModule, RouterModule.forChild(ENTITY_STATES), FormDateTimePickerModule, ArtemisConfirmButtonModule],
+    imports: [
+        ArtemisSharedModule,
+        RouterModule.forChild(ENTITY_STATES),
+        FormDateTimePickerModule,
+        ArtemisConfirmButtonModule,
+        ArtemisMarkdownEditorModule,
+    ],
     declarations: [LectureComponent, LectureDetailComponent, LectureUpdateComponent, LectureAttachmentsComponent],
     entryComponents: [LectureComponent, LectureUpdateComponent, LectureAttachmentsComponent],
+    providers: [LectureService],
 })
 export class ArtemisLectureModule {}
