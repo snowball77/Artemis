6d6aef90a8a509bacbae0deec595c89d5abd659b
Alexander Ungar <alexander.ungar@tum.de>
2020-03-02 18:02:29 +0100

    Import ExerciseHintParticipationModule in programming-exercise-instructions-render.module.ts

--- src/main/webapp/app/exercises/programming/shared/instructions-render/programming-exercise-instructions-render.module.ts
+++ src/main/webapp/app/exercises/programming/shared/instructions-render/programming-exercise-instructions-render.module.ts
@@ -5,9 +5,10 @@ import { ProgrammingExerciseInstructionComponent } from 'app/exercises/programmi
 import { ProgrammingExerciseInstructionStepWizardComponent } from 'app/exercises/programming/shared/instructions-render/step-wizard/programming-exercise-instruction-step-wizard.component';
 import { ProgrammingExerciseInstructionTaskStatusComponent } from 'app/exercises/programming/shared/instructions-render/task/programming-exercise-instruction-task-status.component';
 import { ArtemisMarkdownEditorModule } from 'app/shared/markdown-editor/markdown-editor.module';
+import { ArtemisExerciseHintParticipationModule } from 'app/exercises/shared/exercise-hint/participate/exercise-hint-participation.module';
 
 @NgModule({
-    imports: [ArtemisSharedModule, ArtemisMarkdownEditorModule, ArtemisResultModule],
+    imports: [ArtemisSharedModule, ArtemisMarkdownEditorModule, ArtemisResultModule, ArtemisExerciseHintParticipationModule],
     declarations: [ProgrammingExerciseInstructionComponent, ProgrammingExerciseInstructionStepWizardComponent, ProgrammingExerciseInstructionTaskStatusComponent],
     exports: [ProgrammingExerciseInstructionComponent],
     entryComponents: [ProgrammingExerciseInstructionTaskStatusComponent],
