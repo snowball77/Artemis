73659d11e880f4c6ba23bc514f218e55be39bb14
F4lka <AdrianLoher@gmx.de>
2020-05-21 20:53:47 +0200

    adjusted the implementation of codemirror. Now the general implementation is used instead of the angular wrapper

--- src/main/webapp/app/exercises/programming/shared/code-editor/codemirror/code-editor-codemirror.component.ts
+++ src/main/webapp/app/exercises/programming/shared/code-editor/codemirror/code-editor-codemirror.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, Output, EventEmitter } from '@angular/core';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/swift/swift';
 import 'codemirror/mode/clike/clike';
@@ -7,12 +7,28 @@ import 'codemirror/addon/edit/closebrackets';
 import 'codemirror/addon/edit/matchbrackets';
 import 'codemirror/addon/edit/closetag';
 import * as codeMirror from 'codemirror';
+import { CodemirrorComponent } from '@ctrl/ngx-codemirror/codemirror.component';
+import { Subscription, of, fromEvent } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { CodeEditorRepositoryFileService } from 'app/exercises/programming/shared/code-editor/service/code-editor-repository.service';
+import { TextChange } from 'app/entities/text-change.model';
+import { AnnotationArray } from 'app/entities/annotation.model';
 
 @Component({
     selector: 'jhi-code-editor-codemirror',
     templateUrl: './code-editor-codemirror.component.html',
     styleUrls: ['./code-editor-codemirror.scss'],
 })
-export class CodeEditorCodemirrorComponent {
+export class CodeEditorCodemirrorComponent implements AfterViewInit {
     public content: string;
+
+    ngAfterViewInit(): void {
+        var editor = codeMirror.fromTextArea(document.getElementById('editor-textarea') as HTMLTextAreaElement, {
+            lineNumbers: true,
+            mode: 'text/x-java',
+            lineWrapping: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+        });
+    }
 }
