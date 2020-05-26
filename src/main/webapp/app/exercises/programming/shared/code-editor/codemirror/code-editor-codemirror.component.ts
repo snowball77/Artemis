0e08a033c6ae1659fb51e038380028dbf0ade537
F4lka <AdrianLoher@gmx.de>
2020-05-26 14:29:36 +0200

    imported codemirror

---
+++ src/main/webapp/app/exercises/programming/shared/code-editor/codemirror/code-editor-codemirror.component.ts
@@ -0,0 +1,34 @@
+import { AfterViewInit, Component, Input, Output, EventEmitter } from '@angular/core';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/swift/swift';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/python/python';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closetag';
+import * as codeMirror from 'codemirror';
+
+@Component({
+    selector: 'jhi-code-editor-codemirror',
+    templateUrl: './code-editor-codemirror.component.html',
+    styleUrls: ['./code-editor-codemirror.component.scss'],
+})
+export class CodeEditorCodemirrorComponent implements AfterViewInit {
+    public content: string;
+    ngAfterViewInit(): void {
+        var editor = codeMirror.fromTextArea(document.getElementById('editor-textarea') as HTMLTextAreaElement, {
+            lineNumbers: true,
+            mode: 'text/x-java',
+            lineWrapping: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+            gutters: ['CodeMirror-linenumbers', 'breakpoints'],
+        });
+        editor.on('gutterClick', function (cm, n) {
+            // var info = cm.lineInfo(n);
+            var msg = document.createElement('div');
+            msg.innerText = 'Hier könnte ihr Feedback and Assessment stehen';
+            cm.getDoc().addLineWidget(n, msg, { above: true });
+        });
+    }
+}
