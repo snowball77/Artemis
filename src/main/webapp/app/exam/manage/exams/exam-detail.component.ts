bce420f3ba3f58f5484c5e06c740641cabe2f4d8
Sebastian Jagla <sebastianjagla@gmx.de>
2020-06-24 13:28:00 +0200

    first draft

--- src/main/webapp/app/exam/manage/exams/exam-detail.component.ts
+++ src/main/webapp/app/exam/manage/exams/exam-detail.component.ts
@@ -3,11 +3,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Exam } from 'app/entities/exam.model';
 import { ExamManagementService } from 'app/exam/manage/exam-management.service';
 
+import { CachingStrategy } from 'app/shared/image/secured-image.component';
+
 @Component({
     selector: 'jhi-exam-detail',
     templateUrl: './exam-detail.component.html',
 })
 export class ExamDetailComponent implements OnInit {
+    CachingStrategy = CachingStrategy;
     exam: Exam;
 
     constructor(private route: ActivatedRoute, private examManagementService: ExamManagementService) {}
@@ -17,5 +20,12 @@ export class ExamDetailComponent implements OnInit {
      */
     ngOnInit(): void {
         this.route.data.subscribe(({ exam }) => (this.exam = exam));
+    }
+
+    /**
+     * Go back to the previous page.
+     */
+    previousState() {
+        window.history.back();
     }
 }
